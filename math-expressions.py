ed8273dad2ce775415c643baaebc1a6005e40606
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-11-25 11:09:38 -0300

    feat: finalizando aula 7 - expressões aritméticas

---
+++ math-expressions.py
@@ -0,0 +1,53 @@
+
+# TODO: ------------------------ Desafio --------------------------
+
+# number1 = float(input("Digite o primeiro número decimal: "))
+# number2 = float(input("Digite o segundo número decimal: "))
+# number3 = float(input("Digite o terceiro número decimal: "))
+
+# average = (number1 + number2 + number3) / 3
+# print(f'Números registrados: {number1}, {number2}, {number3}\nMédia: {average}')
+
+
+# TODO: ------------------------ Incrementando e decrementando --------------------------
+# valor = 20
+# print(f'Conteúdo da variável "valor": {valor}')
+# valor = valor - 1
+# print(f'Conteúdo da variável "valor", depois do decremento: {valor}')
+# valor -= 1
+# print(f'Conteúdo da variável "valor", depois do decremento simplificado: {valor}')
+
+# dado = 10
+# print(f'Conteúdo da variável "dado": {dado}')
+# dado = dado + 1
+# print(f'Conteúdo da variável "dado", depois do incremento: {dado}')
+# dado += 1
+# print(f'Conteúdo da variável "dado", depois do incremento simplificado: {dado}')
+
+
+# TODO: ------------------------ Resto de divisão --------------------------
+# numberA = float(input('Digite o primeiro número: '))
+# numberB = float(input('Digite o segundo número: '))
+# modResult = numberA % numberB
+
+# print(f'Resultado do resto da divisão: {modResult}')
+
+# resultado = int(5 / 2)
+# resto = 5 % 2
+
+# print('Quociente: ', resultado)
+# print('Resto da divisão: ', resto)
+
+# TODO: ------------------------ Início --------------------------
+# numberA = float(input('Digite o primeiro número: '))
+# numberB = float(input('Digite o segundo número: '))
+
+# sumResult = numberA + numberB
+# subResult = numberA - numberB
+# multResult = numberA * numberB
+# divResult = numberA / numberB
+
+# print(f'Resultado da soma: {sumResult}')
+# print(f'Resultado da subtração: {subResult}')
+# print(f'Resultado da multiplicação: {multResult}')
+# print(f'Resultado da divisão: {divResult}')
