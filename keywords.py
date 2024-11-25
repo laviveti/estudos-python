7e76f4275e68d0febfdea54880ce31d7a7a30e0e
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-11-25 11:46:24 -0300

    Palavras reservadas (keywords) [Curso de Python: Aula 8]

---
+++ keywords.py
@@ -0,0 +1,7 @@
+
+# ------------------- #TODO Palavras reservadas -------------------------------
+
+# and, as, assert, break, class, continue, def, del, elif, 
+# else, except, exec, finally, False, for, from, global, if, import
+# in, is, lambda, nonlocal, None, not, or,  pass, raise, 
+# return, True, try, with, while, yield, async, await
