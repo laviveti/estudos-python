0e921962fecbf1b919bc64076695214e0ebf2390
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-11-25 08:11:59 -0300

    feat: completando aula 5

---
+++ input.py
@@ -0,0 +1,5 @@
+nome = input('Informe seu nome: ')
+telefone = input('Informe seu telefone: ')
+identidade = input('Informe seu documento: ')
+
+print(f'O usuário possui os seguintes dados: \nNome: {nome}\nTelefone: {telefone}\nIdentidade: {identidade}')
