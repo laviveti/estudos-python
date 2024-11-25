5931287e0c59d3ecf700c73de1fe5c5c69fb5447
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-11-25 17:15:08 -0300

    aula 12: f-elif-else

---
+++ if-elif-else.py
@@ -0,0 +1,47 @@
+# ------------------- #TODO Desafio -------------------------------
+mes = int(input('Digite um número compreendido entre 1 e 12: '))
+
+if mes == 1:
+  print('Janeiro')
+elif mes == 2:
+  print('Fevereiro')
+elif mes == 3:  
+  print('Março')
+elif mes == 4:
+  print('Abril')
+elif mes == 5:
+  print('Maio')
+elif mes == 6:
+  print('Junho')
+elif mes == 7:
+  print('Julho')
+elif mes == 8:
+  print('Agosto')
+elif mes == 9:
+  print('Setembro')
+elif mes == 10:
+  print('Outubro')
+elif mes == 11:
+  print('Novembro')
+elif mes == 12:
+  print('Dezembro')
+
+# ------------------- #TODO Aula -------------------------------
+# dia = int(input('Digite um número compreendido entre 1 e 7: '))
+
+# if dia == 1:
+#   print('Domingo')
+# elif dia == 2:
+#   print('Segunda-feira')
+# elif dia == 3:
+#   print('Terço-feira')
+# elif dia == 4:
+#   print('Quarta-feira')
+# elif dia == 5:
+#   print('Quinta-feira')
+# elif dia == 6:
+#   print('Sexta-feira')
+# elif dia == 7:
+#   print('Sábado')
+# else:
+#   print('Número inválido')
